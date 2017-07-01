0a5184815cedd7e943bef729d8685ed47e4c33e0
ethen8181 <ethen8181@gmail.com>
2017-07-01 16:47:23 -0700

    working on tree explainer

---
+++ recommendation/implicit_feedback/bpr/vbpr.py
@@ -0,0 +1,343 @@
+import sys
+import numpy as np
+import tensorflow as tf
+from tqdm import trange
+from itertools import islice
+from sklearn.base import BaseEstimator
+from sklearn.preprocessing import normalize
+from sklearn.neighbors import NearestNeighbors
+
+class VBPR(BaseEstimator):
+    """
+    Bayesian Personalized Ranking (BPR) for implicit feedback data
+    using tensorflow as backend
+    
+    Inherit scikit-learn's BaseEstimator to get some free stuff
+    - class representations, more informative when printing class
+    - get, set parameters (it will raise ValueError if we pass in
+      illegal parameters, while setattr will simply let it through)
+    
+    TODO : remove the scikit-learn dependency
+    Parameters
+    ----------
+    learning_rate : float, default 0.01
+        learning rate for the Adam optimizer
+    n_factors : int, default 20
+        Number/dimension of user and item latent factors
+    n_iters : int, default 15
+        Number of iterations to train the algorithm
+        
+    n_batch_size : int, default 2000
+        batch size for batch gradient descent, the original paper
+        uses stochastic gradient descent (i.e., batch size of 1),
+        but this can make the training unstable (very sensitive to
+        learning rate)
+    reg : int, default 0.01
+        Regularization term for the user and item latent factors
+    seed : int, default 1234
+        Seed for the randomly initialized user, item latent factors
+    verbose : boolean, default True
+        Whether to print progress bar while training
+    Attributes
+    ----------
+    user_factors : 2d numpy array [n_users, n_factors]
+        User latent factors learnt
+    item_factors : 2d numpy array [n_items, n_factors]
+        Item latent factors learnt
+        
+    item_bias : 1d numpy array [n_items]
+        bias term for the items
+    history : list
+        Loss function's history, can be used to evaluate
+        whether the algorithm converged or not
+    Reference
+    ---------
+    S. Rendle, C. Freudenthaler, Z. Gantner, L. Schmidt-Thieme 
+    Bayesian Personalized Ranking from Implicit Feedback
+    - https://arxiv.org/pdf/1205.2618.pdf
+    """
+    def __init__(self, img_features, learning_rate = 0.01, n_factors = 15, 
+                 n_img_factors = 10, n_iters = 10, n_batch_size = 2000, 
+                 reg = 0.01, reg_img = 0.01, seed = 1234, verbose = True):
+        self.reg = reg
+        self.seed = seed
+        self.reg_img = reg_img
+        self.verbose = verbose
+        self.n_iters = n_iters
+        self.n_factors = n_factors
+        self.n_batch_size = n_batch_size
+        self.n_img_factors = n_img_factors
+        self.learning_rate = learning_rate
+        self.img_features = img_features ## ? parameters or input data
+        
+    def fit(self, ratings):
+        """
+        Parameters
+        ----------
+        ratings : scipy sparse csr_matrix [n_users, n_items]
+            sparse matrix of user-item interactions
+        """
+        # history stores the cost, allows assessing convergence
+        self.history = []
+        indptr = ratings.indptr
+        indices = ratings.indices
+        n_users, n_items = ratings.shape
+        
+        # ensure batch size makes sense
+        batch_size = self.n_batch_size
+        if ratings.nnz < batch_size:
+            batch_size = ratings.nnz
+            sys.stderr.write('WARNING: Batch size is greater than number of training interactions,'
+                             'switching to a batch size of {}\n'.format(X.nnz))
+
+        batch_iters = ratings.nnz // batch_size
+        
+        # progress bar for training iteration if verbose is turned on
+        loop = range(self.n_iters)
+        if self.verbose:
+            loop = trange(self.n_iters, desc = self.__class__.__name__)
+        
+        self._build_graph(n_users, n_items)
+        init = tf.global_variables_initializer()
+        
+        with tf.Session() as sess:
+            sess.run(init)
+
+            for _ in loop:
+                iteration_cost = 0.0
+                for _ in range(batch_iters):
+                    sampled = self._sample(n_users, n_items, indices, indptr)
+                    sampled_users, sampled_pos_items, sampled_neg_items = sampled
+                    feed_dict = {self._slice_u: sampled_users, 
+                                 self._slice_i: sampled_pos_items, 
+                                 self._slice_j: sampled_neg_items}
+                    _, cost = sess.run([self._train_step, self._total_cost], feed_dict)
+                    iteration_cost += cost / self.n_batch_size
+
+                iteration_cost /= batch_iters
+                self.history.append(iteration_cost)
+
+            self.user_factors = sess.run(self.user_factors)
+            self.item_factors = sess.run(self.item_factors)
+            self.item_bias = sess.run(self.item_bias)
+            self.user_img_factors = sess.run(self.user_img_factors)
+            self.img_embedding = sess.run(self.img_embedding)
+            self.img_bias = sess.run(self.img_bias)
+        
+        return self
+        
+    def _sample(self, n_users, n_items, indices, indptr):
+        """sample batches of random triplets u, i, j"""
+        sampled_pos_items = np.zeros(self.n_batch_size, dtype = np.int32)
+        sampled_neg_items = np.zeros(self.n_batch_size, dtype = np.int32)
+        sampled_users = np.random.choice(n_users, size = self.n_batch_size).astype(np.int32)
+
+        for idx, user in enumerate(sampled_users):
+            pos_items = indices[ indptr[user]:indptr[user + 1] ]
+            pos_item = np.random.choice(pos_items)
+            neg_item = np.random.choice(n_items)
+            while neg_item in pos_items:
+                neg_item = np.random.choice(n_items)
+
+            sampled_pos_items[idx] = pos_item
+            sampled_neg_items[idx] = neg_item
+
+        return sampled_users, sampled_pos_items, sampled_neg_items       
+    
+    def _build_graph(self, n_users, n_items):
+        """build the tensorflow computational graph"""
+        # initialize random weights
+        n_img_features = self.img_features.shape[1]
+        user_init = tf.truncated_normal((n_users, self.n_factors), seed = self.seed)
+        item_init = tf.truncated_normal((n_items, self.n_factors), seed = self.seed)
+        user_img_init = tf.truncated_normal((n_users, self.n_img_factors), seed = self.seed)
+        img_bias_init = tf.truncated_normal((n_users, n_img_features), seed = self.seed)
+        img_embedding_init = tf.truncated_normal((n_img_features, self.n_img_factors), seed = self.seed)
+        self.user_factors = tf.Variable(user_init, name = 'user_factors')
+        self.item_factors = tf.Variable(item_init, name = 'item_factors')
+        self.item_bias = tf.Variable(tf.zeros(n_items), name = 'item_bias')
+        self.user_img_factors = tf.Variable(user_img_init, name = 'user_img_factors')
+        self.img_embedding = tf.Variable(img_embedding_init, name = 'img_embedding')
+        self.img_bias = tf.Variable(img_bias_init, name = 'img_bias')
+        img_features = tf.Variable(self.img_features, name = 'img_features')
+
+        # the input data is the sampled batch of users index,
+        # its corresponding positive and negative items' index and
+        # image features of the positive and negative items
+        self._slice_u = tf.placeholder(tf.int32, self.n_batch_size)
+        self._slice_i = tf.placeholder(tf.int32, self.n_batch_size)
+        self._slice_j = tf.placeholder(tf.int32, self.n_batch_size)
+
+        # use tf.gather() to select a non-contiguous slice from the tensor
+        # http://stackoverflow.com/questions/35146444/tensorflow-python-accessing-individual-elements-in-a-tensor
+        img_u  = tf.gather(self.user_img_factors, self._slice_u)
+        user_u = tf.gather(self.user_factors, self._slice_u)
+        item_i = tf.gather(self.item_factors, self._slice_i)
+        item_j = tf.gather(self.item_factors, self._slice_j)
+        bias_i = tf.gather(self.item_bias, self._slice_i)
+        bias_j = tf.gather(self.item_bias, self._slice_j)
+        img_bias_u = tf.gather(self.img_bias, self._slice_u)
+        img_feature_i = tf.transpose( tf.gather(img_features, self._slice_i) )
+        img_feature_j = tf.transpose( tf.gather(img_features, self._slice_j) )
+
+        # decompose the estimator, compute the difference between
+        # the score of the positive items i and negative items j
+        temp_img_uij = tf.matmul(tf.transpose(self.img_embedding), img_feature_i - img_feature_j)
+        img_uij = tf.diag_part( tf.matmul(img_u, temp_img_uij) )
+        r_uij = tf.diag_part( tf.matmul(user_u, tf.transpose(item_i - item_j)) )
+        img_bias_uij = tf.diag_part( tf.matmul(img_bias_u, img_feature_i - img_feature_j) )
+        diff = bias_i - bias_j + r_uij + img_uij + img_bias_uij
+
+        # minimize the cost
+        cost_u = self.reg * tf.reduce_sum(user_u ** 2)
+        cost_i = self.reg * tf.reduce_sum(item_i ** 2) + tf.reduce_sum(bias_i ** 2)
+        cost_j = self.reg * tf.reduce_sum(item_j ** 2) + tf.reduce_sum(bias_j ** 2)
+        cost_img = self.reg_img * ( tf.reduce_sum(self.img_embedding ** 2) + tf.reduce_sum(img_u ** 2) ) + tf.reduce_sum(img_bias_u ** 2)
+        
+        output = tf.clip_by_value(tf.nn.sigmoid(diff), 1e-10, 1.0)
+        cost_uij = tf.reduce_sum( tf.log(output) )
+        self._total_cost = cost_u + cost_i + cost_j + cost_img - cost_uij
+        optimizer = tf.train.AdamOptimizer(self.learning_rate)
+        self._train_step = optimizer.minimize(self._total_cost)
+        return self
+
+    def predict(self):
+        """
+        Obtain the predicted ratings for every users and items
+        by doing a dot product of the learnt user and item vectors.
+        The result will be cached to avoid re-computing 
+        it every time we call predict, thus there will
+        only be an overhead the first time we call it.
+        Note, ideally you probably don't need to compute 
+        this as it returns a dense matrix and may take
+        up huge amounts of memory for large datasets
+        """
+        if not self._predicted:
+            self._get_prediction()
+            self._predicted = True
+
+        return self._pred
+
+    def _get_prediction(self):
+        """Predicted ratings (dot product of user and item vectors)"""
+        self._pred = self.user_factors.dot(self.item_factors.T) + self.item_bias
+        temp = self.user_img_factors.dot(self.img_embedding.T)
+        self._pred += temp.dot(self.img_features.T)
+        self._pred += self.img_bias.dot(self.img_features.T)
+        return self
+
+    def _predict_user(self, user):
+        """
+        returns the predicted ratings for the specified user,
+        this is mainly used in computing evaluation metric,
+        where we avoid computing the whole predicted rating matrix
+        
+        TODO : do we even need this in the class?
+        """
+        user_pred = self.user_factors[user].dot(self.item_factors.T) + self.item_bias
+        temp = self.user_img_factors[user].dot(self.img_embedding.T)
+        user_pred += temp.dot(self.img_features.T)
+        user_pred += self.img_bias[user].dot(self.img_features.T)
+        return user_pred
+    
+    def recommend(self, ratings, N = 5, user_ids = None):
+        """
+        Returns the top N ranked items for given user id,
+        excluding the ones that the user already liked
+        
+        Parameters
+        ----------
+        ratings : scipy sparse csr_matrix [n_users, n_items]
+            sparse matrix of user-item interactions 
+        
+        N : int, default 5
+            top-N similar items' N
+            
+        user_ids : 1d iterator, e.g. list or numpy array, default None
+            users' id that we wish to find top-N recommended for,
+            default None will find it for all users
+        
+        Returns
+        -------
+        recommendation : 2d numpy array [number of query users_ids, N]
+            each row is the top-N ranked item for each query user
+        """
+        if user_ids is not None:
+            recommendation = np.zeros((len(user_ids), N), dtype = np.int32)
+            for idx, user in enumerate(user_ids):
+                top_n = self._recommend_user(ratings, user, N)
+                recommendation[idx] = top_n
+        else:
+            n_users = ratings.shape[0]
+            recommendation = np.zeros((n_users, N), dtype = np.int32)
+            for user in range(n_users):
+                top_n = self._recommend_user(ratings, user, N)
+                recommendation[user] = top_n
+
+        return recommendation
+
+    def _recommend_user(self, ratings, user, N):
+        """the top-N ranked items for a given user"""
+        scores = self._predict_user(user)
+
+        # compute the top N items, removing the items that the user already liked
+        # from the result and ensure that we don't get out of bounds error when 
+        # we ask for more recommendations than that are available
+        liked = set(ratings[user].indices)
+        count = N + len(liked)
+        if count < scores.shape[0]:
+
+            # when trying to obtain the top-N indices from the score,
+            # using argpartition to retrieve the top-N indices in 
+            # unsorted order and then sort them will be faster than doing
+            # straight up argort on the entire score
+            # http://stackoverflow.com/questions/42184499/cannot-understand-numpy-argpartition-output
+            ids = np.argpartition(scores, -count)[-count:]
+            best_ids = np.argsort(scores[ids])[::-1]
+            best = ids[best_ids]
+        else:
+            best = np.argsort(scores)[::-1]
+
+        top_n = list( islice((rec for rec in best if rec not in liked), N) )
+        return top_n
+    
+    def get_similar_items(self, N = 5, item_ids = None):
+        """
+        return the top N similar items for itemid, where
+        cosine distance is used as the distance metric
+        
+        Parameters
+        ----------
+        N : int, default 5
+            top-N similar items' N
+            
+        item_ids : 1d iterator, e.g. list or numpy array, default None
+            the item ids that we wish to find the similar items
+            of, the default None will compute the similar items
+            for all the items
+        
+        Returns
+        -------
+        similar_items : 2d numpy array [number of query item_ids, N]
+            each row is the top-N most similar item id for each
+            query item id
+        """
+        # cosine distance is proportional to normalized euclidean distance,
+        # thus we normalize the item vectors and use euclidean metric so
+        # we can use the more efficient kd-tree for nearest neighbor search;
+        # also the item will always to nearest to itself, so we add 1 to 
+        # get an additional nearest item and remove itself at the end
+        normed_item_factors = normalize(self.item_factors)
+        knn = NearestNeighbors(n_neighbors = N + 1, metric = 'euclidean')
+        knn.fit(normed_item_factors)
+
+        # returns a distance, index tuple,
+        # we don't actually need the distance
+        if item_ids is not None:
+            normed_item_factors = normed_item_factors[item_ids]
+
+        _, items = knn.kneighbors(normed_item_factors)
+        similar_items = items[:, 1:].astype(np.int32)
+        return similar_items
+
+
---
+++ tree_explainer/tree_explainer.py
@@ -0,0 +1,208 @@
+import numpy as np
+import pandas as pd
+from joblib import Parallel, delayed
+from sklearn.tree import _tree
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+
+
+def _format_explanation(model, data_row, prediction, contrib, feature_names):
+    if isinstance(model, (DecisionTreeClassifier, RandomForestClassifier)):
+        best_idx = np.argmax(prediction)
+        contrib = contrib[:, best_idx]
+
+    # convert the explanation to dataframe for better interpretation,
+    # column names are hard-coded
+    value_col = 'value'
+    contrib_col = 'contrib'
+    feature_col = 'feature'
+    explained = {value_col: data_row,
+                 contrib_col: contrib,
+                 feature_col: feature_names}
+    df_explained = pd.DataFrame(explained, columns = [contrib_col, feature_col, value_col])
+    df_explained = (df_explained
+                    .loc[df_explained[contrib_col] != 0.0]
+                    .sort_values(contrib_col, ascending = False)
+                    .reset_index(drop = True))
+
+    pred_info = {'predict': prediction}
+    if isinstance(model, (DecisionTreeClassifier, RandomForestClassifier)):
+        pred_info = {'predict': best_idx,
+                     'predict_proba': prediction}
+
+    return df_explained, pred_info
+
+
+class DecisionTreeExplainer:
+    """docstring for ClassName"""
+    def __init__(self, model_tree, feature_names):
+        self.model_tree = model_tree
+        self.feature_names = feature_names
+
+        # map leaves to paths, and reverse the path so
+        # that the sequence starts with the root node
+        paths = self._get_tree_paths(tree = self.model_tree.tree_, node_id = 0)
+        leaf_to_path = {}
+        for path in paths:
+            path.reverse()
+            leaf_to_path[path[-1]] = path
+
+        # remove the single-dimensional inner arrays
+        values = self.model_tree.tree_.value.squeeze()
+        if isinstance(self.model_tree, DecisionTreeRegressor):
+            contribs_shape = self.model_tree.n_features_
+        elif isinstance(self.model_tree, DecisionTreeClassifier):
+            # sklearn classification tree's value stores class counts
+            # at each node id, we turn them into probabilities
+            normalizer = np.sum(values, axis = 1, keepdims = 1)
+            values /= normalizer
+            contribs_shape = self.model_tree.n_features_, self.model_tree.n_classes_
+
+        self._unique_contribs = self._compute_contrib(values, leaf_to_path, contribs_shape)
+
+        # the bias is always simply the value at the root node
+        self._values = values
+        self.bias_ = values[0]
+
+        # TODO: the attribute is used to return different value
+        # when using the class in the RandomForestExplainer;
+        # might need to evaluate whether this is the best way
+        self._ensemble_tree = False
+
+    def explain(self, data_row):
+        """
+        Parameters
+        ---------
+        data_row: 1d nd.array
+            corresponding to a row
+        """
+
+        # return the index of the leaf each data points ends up in
+        # and obtain prediction for each observation
+        # by using which leaf it belongs to;
+        # prediction is essentially the same as calling
+        # self.model_tree.predict_proba(X) for classification tree
+        # or self.model_tree.predict(X) for regression tree
+        X = np.atleast_2d(data_row)
+        leaf = self.model_tree.apply(X)[0]
+        contrib = self._unique_contribs[leaf]
+        prediction = self._values[leaf]
+
+        if not self._ensemble_tree:
+            df_explained, pred_info = _format_explanation(
+                self.model_tree, data_row, prediction, contrib, self.feature_names)
+
+            return df_explained, pred_info
+        else:
+            return prediction, contrib, self.bias_
+
+    def _get_tree_paths(self, tree, node_id, depth = 0):
+        """
+        returns all paths through the tree as list
+        of node_ids, note that the path here will
+        be the sequence of nodes from the leaf node
+        to the root node
+        """
+        left_node = tree.children_left[node_id]
+        right_node = tree.children_right[node_id]
+
+        if left_node != _tree.TREE_LEAF:
+            left_paths = self._get_tree_paths(tree, left_node, depth + 1)
+            right_paths = self._get_tree_paths(tree, right_node, depth + 1)
+
+            for path in left_paths:
+                path.append(node_id)
+
+            for path in right_paths:
+                path.append(node_id)
+
+            paths = left_paths + right_paths
+        else:
+            paths = [[node_id]]
+
+        return paths
+
+    def _compute_contrib(self, values, leaf_to_path, contribs_shape):
+        """
+        compute the contribution vector for each unique tree leaf nodes
+        and store the result into a dictionary, whose keys are leaf nodes
+        and the corresponding value refers to the contribution vector of the leaf node.
+        after that we can simply assign the contribution vector to each observation
+        by looking up which leaf node it is assigned
+        """
+
+        # convert numpy array into python list,
+        # accessing values will be faster
+        # https://stackoverflow.com/questions/35020604/why-is-numpy-list-access-slower-than-vanilla-python
+        values = list(values)
+
+        # feature[i] holds the feature to split on,
+        # for the internal node i
+        feature = list(self.model_tree.tree_.feature)
+
+        unique_contribs = {}
+        for leaf, path in leaf_to_path.items():
+            # compute the contribution of each feature
+            # for a given observation
+            contribs = np.zeros(contribs_shape)
+            for depth in range(len(path) - 1):
+                contrib = values[path[depth + 1]] - values[path[depth]]
+                feature_idx = feature[path[depth]]
+                contribs[feature_idx] += contrib
+
+            unique_contribs[leaf] = contribs
+
+        return unique_contribs
+
+
+class RandomForestExplainer:
+    """docstring for ClassName"""
+    def __init__(self, model_rf, feature_names, n_jobs = -1,
+                 verbose = True, pre_dispatch = '2*n_jobs'):
+        self.n_jobs = n_jobs
+        self.verbose = verbose
+        self.model_rf = model_rf
+        self.pre_dispatch = pre_dispatch
+        self.feature_names = feature_names
+
+    def explain(self, data_row):
+        """
+        Parameters
+        ---------
+        data_row: 1d nd.array
+            corresponding to a row
+        """
+
+        # average the contribution for each individual trees
+        parallel = Parallel(n_jobs = self.n_jobs, verbose = self.verbose,
+                            pre_dispatch = self.pre_dispatch)
+        output = parallel(delayed(self._explain_model_tree)(data_row, model_tree,
+                                                            self.feature_names)
+                          for model_tree in self.model_rf.estimators_)
+
+        predictions, contribs, biases = zip(*output)
+
+        if isinstance(self.model_rf, RandomForestClassifier):
+            self.bias_ = np.mean(biases, axis = 0)
+            prediction = np.mean(predictions, axis = 0)
+        elif isinstance(self.model_rf, RandomForestRegressor):
+            self.bias_ = np.mean(biases)
+            prediction = np.mean(predictions)
+
+        contrib = np.mean(contribs, axis = 0)
+
+        df_explained, pred_info = _format_explanation(
+            self.model_rf, data_row, prediction, contrib, self.feature_names)
+
+        return df_explained, pred_info
+
+    def _explain_model_tree(self, data_row, model_tree, feature_names):
+        # modify the private attribute so we can get all the information that
+        # is needed for the ensemble tree explainer
+        tree_explain = DecisionTreeExplainer(model_tree, self.feature_names)
+        tree_explain._ensemble_tree = True
+        prediction, contrib, bias = tree_explain.explain(data_row)
+        return prediction, contrib, bias
+
+
+__all__ = [DecisionTreeExplainer, RandomForestExplainer]
